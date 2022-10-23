3664a25db6f737563085e2daa040cb1119f9240e
ycuervob <ycuervob@unal.edu.co>
2022-10-23 00:33:35 -0500

    App bar easy close

--- src/Pages/Appbar.jsx
+++ src/Pages/Appbar.jsx
@@ -1,47 +1,59 @@
 import * as React from 'react';
-import { Text, View, ImageBackground, Image } from 'react-native';
+import { Text, View, ImageBackground, Image, TouchableOpacity, Alert } from 'react-native';
 import { Appbar, Drawer } from 'react-native-paper';
 import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
 import { faBars } from '@fortawesome/free-solid-svg-icons';
 
 
-const SidePanel = ({ setPage, page }) => {
+const SidePanel = ({ setPage, page, active, setActive }) => {
+
+    const ontouchoutside = () => {
+        setActive(false);
+    }
+
+    const changePage = (namePage) =>{
+        setPage(namePage);
+        setActive(false);
+    }
+
     return (
-        <View style={{ flexDirection: "column", justifyContent: 'space-between', position: 'absolute', backgroundColor: '#FFF', paddingBottom: "10%", paddingTop: "10%", elevation: 6, right: "20%", left: 0, top: 63, bottom: 0 }}>
-            <Drawer.Section>
-                <Drawer.Item
-                    label="Información Personal"
-                    active={page === 'Info_personal'}
-                    onPress={() => setPage('Info_personal')}
-                />
-                <Drawer.Item
-                    label="Información Académica"
-                    active={page === 'Info_academica'}
-                    onPress={() => setPage('Info_academica')}
-                />
-                <Drawer.Item
-                    label="Horario"
-                    active={page === 'Horario'}
-                    onPress={() => setPage('Horario')}
-                />
-            </Drawer.Section>
-            <View>
-                <Image style={{
-                    alignContent: "center",
-                    marginLeft: "auto",
-                    marginRight: "auto",
-                }} source={require('../Assets/Images/logo.png')} />
-                <Text style={{ padding: 20 }}>
-                    Contacto página web
-                    Cra. 1 No. 18-10, Edificio 2, Piso 2, Oficina 201
-                    Bogotá, Colombia
-                    Teléfono: (57) 1 381 8000
-                    Fax: (57) 1 381 8001
-                    Correo electrónico:
-                    iami@gmail.com
-                </Text>
-            </View>
-        </View >
+        <TouchableOpacity onPressIn={ontouchoutside} style={{ position: 'absolute', backgroundColor: '#FFF', paddingBottom: "10%", paddingTop: "10%", elevation: 6, right: "20%", left: 0, top: 63, bottom: 0 }}>
+            <View style={{ flex: 1, flexDirection: "column", justifyContent: 'space-between' }}>
+                <Drawer.Section>
+                    <Drawer.Item
+                        label="Información Personal"
+                        active={page === 'Info_personal'}
+                        onPress={() => { changePage('Info_personal') }}
+                    />
+                    <Drawer.Item
+                        label="Información Académica"
+                        active={page === 'Info_academica'}
+                        onPress={() => changePage('Info_academica')}
+                    />
+                    <Drawer.Item
+                        label="Horario"
+                        active={page === 'Horario'}
+                        onPress={() => changePage('Horario')}
+                    />
+                </Drawer.Section>
+                <View>
+                    <Image style={{
+                        alignContent: "center",
+                        marginLeft: "auto",
+                        marginRight: "auto",
+                    }} source={require('../Assets/Images/logo.png')} />
+                    <Text style={{ padding: 20 }}>
+                        Contacto página web
+                        Cra. 1 No. 18-10, Edificio 2, Piso 2, Oficina 201
+                        Bogotá, Colombia
+                        Teléfono: (57) 1 381 8000
+                        Fax: (57) 1 381 8001
+                        Correo electrónico:
+                        iami@gmail.com
+                    </Text>
+                </View>
+            </View >
+        </TouchableOpacity>
     );
 }
 
@@ -50,6 +62,10 @@ const myApp = ({ children }) => {
     const [active, setActive] = React.useState(false);
     const [page, setPage] = React.useState('');
     const names = { Info_personal: 1 };
+
+    const onsreent = () => {
+        setActive(false);
+    }
 
     return (
         <View style={{ flex: 1 }}>
@@ -64,11 +80,11 @@ const myApp = ({ children }) => {
                     </Appbar>
                 </View>
                 <View style={{ flex: 1, justifyContent: "center" }}>
-                    <View style={{ backgroundColor: "#FFF", borderRadius:10}}>
+                    <View style={{ backgroundColor: "#FFF", borderRadius: 10 }}>
                         {children[names[page] || 0]}
                     </View>
                 </View>
-                {active ? <SidePanel setPage={setPage} page={page} /> : <></>}
+                {active ? <SidePanel setPage={setPage} setActive={setActive} active={active} page={page} /> : <></>}
             </ImageBackground>
         </View>
     );
